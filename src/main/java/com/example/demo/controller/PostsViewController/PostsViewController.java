072f6cf259c8b04946eac79d558fe691766a4a80
Shevtsova-Ilona <Telez_2006@mail.ru>
2026-02-22 00:48:57 +0300

    Коммит

--- src/main/java/com/example/demo/controller/PostsViewController/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController/PostsViewController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller.PostsViewController;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +9,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
-    }
+//    @ResponseBody
+//    @RequestMapping(path = "/", method = RequestMethod.GET)
+//    public String list() {
+//        return "Здесь будет главная страница";
+//    }
 
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
 }
